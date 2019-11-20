7ab54381bffd49da838513059a4ca764e63c4c37
Christian Elies <chris.elies13@gmail.com>
2019-11-20 21:36:20 +0100

    refactor(): refactored on move and on delete to view modifiers

---
+++ Sources/AdvancedList/private/Models/AnyDynamicViewContent.swift
@@ -0,0 +1,22 @@
+//
+//  AnyDynamicViewContent.swift
+//  
+//
+//  Created by Christian Elies on 20.11.19.
+//
+
+import SwiftUI
+
+extension AdvancedList {
+    struct AnyDynamicViewContent: DynamicViewContent {
+        private let view: AnyView
+
+        private(set) var data: AnyCollection<Any>
+        var body: some View { view }
+
+        init<View: DynamicViewContent>(_ view: View) {
+            self.view = AnyView(view)
+            self.data = AnyCollection(view.data.map { $0 as Any })
+        }
+    }
+}

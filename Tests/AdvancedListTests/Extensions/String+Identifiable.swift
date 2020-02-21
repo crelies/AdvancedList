10a12e4f811720ef24fe25054203267b544f29ce
Christian Elies <chris.elies13@gmail.com>
2020-02-21 23:16:54 +0100

    test(): implemented some simple unit tests

---
+++ Tests/AdvancedListTests/Extensions/String+Identifiable.swift
@@ -0,0 +1,10 @@
+//
+//  String+Identifiable.swift
+//  AdvancedListTests
+//
+//  Created by Christian Elies on 21.02.20.
+//
+
+extension String: Identifiable {
+    public var id: String { self }
+}
